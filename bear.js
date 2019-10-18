b066d53dc1642d2e0aa789703681d9c997fe2aa1
vic-techwriter <drexler22@protonmail.com>
2019-10-17 20:43:32 -0500

    added javascript to update date

---
+++ bear.js
@@ -0,0 +1,10 @@
+//Update Date
+
+window.onload = function() {
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];;
+    var date = new Date();
+    var city = 'Austin, TX'
+    var social = '<a href="https://twitter.com/Vic_techcomm">Twitter</a>'
+
+    document.getElementById('contact').innerHTML = city + ' | ' + months[date.getMonth()] + '  ' + date.getFullYear() + ' | ' + social;
+};
