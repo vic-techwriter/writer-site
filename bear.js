6f76d42a4bbcdd29c6854fbc4339fb577622bc6d
vic-techwriter <drexler22@protonmail.com>
2020-08-11 20:24:07 -0500

    update to home page

--- bear.js
+++ bear.js
@@ -1,11 +1,11 @@
 //Update Date
 
 window.onload = function() {
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];;
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     var date = new Date();
-    var calendar = months[date.getMonth()] + '  ' + date.getFullYear()
-    var city = 'Austin, TX'
-    var social = '<a href="https://twitter.com/Vic_techcomm">Twitter</a>'
+    var calendar = months[date.getMonth()] + '  ' + date.getFullYear();
+    var city = 'Austin, TX';
+    var social = '<a href="https://twitter.com/Vic_techcomm">Twitter</a>';
 
     document.getElementById('contact').innerHTML = city + ' | ' + calendar + ' | ' + social;
 };
