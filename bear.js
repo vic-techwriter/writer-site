6bafa89c43780322a2c90e60e616adeb80d85388
vic-techwriter <drexler22@protonmail.com>
2020-08-22 20:02:38 -0500

    updated links

--- bear.js
+++ bear.js
@@ -5,7 +5,7 @@ window.onload = function() {
     var date = new Date();
     var calendar = months[date.getMonth()] + '  ' + date.getFullYear();
     var city = 'Austin, TX';
-    var social = '<a href="https://twitter.com/Vic_techcomm">Contact</a>';
+    var social = '<a data-toggle="modal" href="#contactModal" aria-expanded="false" aria-controls="contactModal">Contact</a>';
 
     document.getElementById('contact').innerHTML = city + ' | ' + calendar + ' | ' + social;
 };
