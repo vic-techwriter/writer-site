69ab401e68dbb22d7b0127a333b1043d24f9210f
vic-techwriter <drexler22@protonmail.com>
2021-02-27 10:29:37 -0600

    header

--- bear.js
+++ bear.js
@@ -6,7 +6,7 @@ window.onload = function() {
     var keywords = '<meta name="keywords" content="DITA, XML, technical writing, austin, texas, documentation, structured content, victor estrada, technical communication">';
     var author = '<meta name="author" content="Victor Estrada">';
     var viewport = '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';
-    var js = '<script type="text/javascript" src="/bear.js"></script>';
+    var js = '<script type="text/javascript" src="bear.js"></script>';
     var bootstrap = '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
     var font = '<link href="https://fonts.googleapis.com/css?family=Quicksand|Anton|Lora&display=swap" rel="stylesheet">';
     var style = '<link rel="stylesheet" href="styles.css">';
--- writer-resume.xsl
+++ writer-resume.xsl
@@ -4,12 +4,7 @@
 
 <xsl:template match="/overview">
   <html>
-    <head id="header">
-      <script type="text/javascript" src="bear.js"></script>
-      <link rel="stylesheet" href="styles.css" />
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
-      <title>Victor Estrada - Technical Communicator</title>
-    </head>
+    <head id="header"></head>
     <body>
       <div class="pages container-fluid">
         <div class="row">
