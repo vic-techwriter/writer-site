e0488c5b44e90297366e02033de4da5c16d70085
Victor Estrada <drexler22@protonmail.com>
2022-03-27 15:04:44 -0500

    footer fix

--- bear-xml.js
+++ bear-xml.js
@@ -13,6 +13,15 @@ window.onload = function() {
     var title = '<title>Victor Estrada - Technical Communicator</title>';
 
     document.getElementById('header').innerHTML = charset + description + keywords + author + viewport + js + bootstrap + font + style + title;
+
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var date = new Date();
+    var calendar = months[date.getMonth()] + '  ' + date.getFullYear();
+    var city = 'Austin, TX';
+    var social = '<a data-toggle="modal" href="#contactModal" aria-expanded="false" aria-controls="contactModal">Contact</a>';
+
+    document.getElementById('contact').innerHTML = city + ' | ' + calendar + ' | ' + social;
+
 };
 
 
